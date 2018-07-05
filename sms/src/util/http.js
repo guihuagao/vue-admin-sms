d29b3b6484c058bc568436a46634a3a8b521ee66
melody <2475352924@qq.com>
2018-07-05 16:10:13 +0800

    Update http.js

--- sms/src/util/http.js
+++ sms/src/util/http.js
@@ -16,6 +16,60 @@ class Axios{
   init(){
    /* axios.defaults.withCredentials = true;*/
     axios.defaults.baseURL = "http://192.168.0.151:9999";
+    axios.interceptors.response.use(response=>{
+      return response
+    },error => {
+      if(error&&error.response.status){
+        switch (error.response.status){
+          case 400:
+          alert("错误请求");
+          break;
+          case 401:
+            alert("未授权或已过期，请重新登录");
+            break;
+          case 403:
+            alert("拒绝访问");
+            break;
+          case 404:
+            alert("请求错误，未找到该资源");
+            break;
+          case 405:
+            alert("请求方法未允许");
+            break;
+          case 408:
+            alert("请求超时");
+            break;
+          case 500:
+            alert("服务器端出错");
+            break;
+          case 501:
+            alert("网络未实现");
+            break;
+          case 502:
+            alert("网络错误");
+            break;
+          case 503:
+            alert("服务不可用");
+            break;
+          case 504:
+            alert("网络超时");
+            break;
+          case 505:
+            alert("网络错误");
+            break;
+          default:
+            alert('连接错误'+error.response.status);
+        }
+      }else{//跨域获取不到状态码或者其他状态码
+        alert("网络出现问题，请稍候再试");
+      }
+    })
+
+
+    
+
+
+
   };
   url(){
     return axios.defaults.baseURL;
